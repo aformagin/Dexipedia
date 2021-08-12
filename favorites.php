b9c990f22e605fc7b0f6f9147b84110db82c097d
juliagarant <jgarant10@gmail.com>
2021-08-11 21:04:25 -0400

    Extra comment that was bothering me

--- favorites.php
+++ favorites.php
@@ -24,7 +24,6 @@ if (!isset($_SESSION['id'])) {
     </head>
     <body >
         <!--This is the start of the Nav bar-->
-        <!--This is the start of the Nav bar-->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="index.php"><img src="imgs/dexipedia.png" style="max-height: 75px"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
